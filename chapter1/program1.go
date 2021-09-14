d679c0717fdde932a95799015d65d3a5a9593d3f
Nikhil2001 <nikhilreddymara@gmail.com>
2021-09-14 10:56:45 +0200

    read and write

--- chapter1/program1.go
+++ chapter1/program1.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
-
 func main() {
+
+	//Write and read data 
 	fmt.Println("Hello World!")
+	var inputUser1, inputUser2 string
+	fmt.Print("Enter input string:")
+	fmt.Scanln(&inputUser1,&inputUser2)
+	fmt.Println(inputUser1)
+	fmt.Println(inputUser2)
+
 }
