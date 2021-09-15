ca9a7b550b6aa079469e1db0ab84f8e005bcc474
Nikhil2001 <nikhilreddymara@gmail.com>
2021-09-15 05:11:52 +0200

    add errors

---
+++ chapter2/error.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"strconv"
+)
+
+func check(a,b int) error {
+	if a==0 && b ==0 {
+		return errors.New("both a and b are zero")
+	}
+
+	if a==0 || b ==0 {
+		return fmt.Errorf("either %v  or %v are zero",a,b)
+	}
+	return nil
+}
+func main() {
+
+	err := check(0,0)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(err.Error() == "both a and b are zero" )
+	intValue,err := strconv.Atoi("123")
+	fmt.Println(intValue,err)
+
+	_,err = strconv.Atoi("a123")
+	fmt.Println(intValue,err)
+
+	err = check(5,0)
+	fmt.Println(err.Error() == "either 5  or 0 are zero" )
+
+	fmt.Printf("the data type is %T\n",err)
+}
