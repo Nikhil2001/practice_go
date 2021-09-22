3da12aa91e1d43d978fcb378e5ede88ee0ade42a
Nikhil2001 <nikhilreddymara@gmail.com>
2021-09-22 05:59:51 +0200

    formated

--- chapter2/intString.go
+++ chapter2/intString.go
@@ -1,32 +1,30 @@
 package main
 
-import ( "fmt"
-          "strconv"
-		  "unicode"
-		_  "os"
+import (
+        "fmt"
+        _ "os"
+        "strconv"
+        "unicode"
 )
 
+func main() {
 
+        //      if len(os.Args) != 2 {
+        //              fmt.Println("enter integer value.")
+        //              return
+        //      }
 
-func main(){
+        fmt.Println(string(49)) //converts int to unicode point, prints 1
 
-//	if len(os.Args) != 2 {
-//		fmt.Println("enter integer value.")
-//		return
-//	}
+        value, _ := strconv.Atoi("333")
+        fmt.Println(value)
 
-fmt.Println(string(49)) //converts int to unicode point, prints 1
+        value, _ = strconv.Atoi("ab")
+        fmt.Println(value)
 
-value,_ := strconv.Atoi("333")
-fmt.Println(value)
+        value1 := strconv.Itoa(7)
+        fmt.Println(value1 + " man")
 
-value,_ = strconv.Atoi("ab")
-fmt.Println(value)
-
-value1  := strconv.Itoa(7)
-fmt.Println(value1+" man")
-
-fmt.Printf("%T %[1]v %c %c\n",unicode.IsPrint(0x99),0x50,0x23)
-
+        fmt.Printf("%T %[1]v %c %c\n", unicode.IsPrint(0x99), 0x50, 0x23)
 
 }
