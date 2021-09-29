63e3974bf9024c06f582dcbab846345ed7e9e639
Nikhil2001 <nikhilreddymara@gmail.com>
2021-09-29 14:18:32 +0200

    update

--- book/chapter5/functions.go
+++ book/chapter5/functions.go
@@ -5,11 +5,23 @@ import (
 	"fmt"
 )
 
+type Any interface{}
+
 func divide(a, b int) (int, error) {
 	if b == 0 {
 		return 0, errors.New("Undefined")
 	}
 	return a / b, nil
+}
+
+func sumAndMul(a, b int) (sum, mul int) {
+	sum = a + b
+	mul = a * b
+	return
+}
+
+func PrintAnything(v ...interface{}) {
+	fmt.Printf("type %T,value %[1]v\n",v)
 }
 
 func main() {
@@ -33,5 +45,16 @@ func main() {
 	if err == nil {
 		fmt.Println(res)
 	}
+
+	fmt.Println(sumAndMul(2, 5))
+	add, mul := sumAndMul(20, 5)
+	fmt.Println(add, mul)
+
+	PrintAnything("nikhil",1)
+
+	slice1 := []Any{1,2,3,"thing"}
+	fmt.Println(slice1)
+	PrintAnything(slice1)
+
 
 }
